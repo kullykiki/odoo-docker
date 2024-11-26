155fd837980835c9d8027c359d17c630c82b4f2a
kullykiki <nexus5onlykully@gmail.com>
2024-11-26 22:34:47 +0700

    add prompt font

--- addons/lsr/__manifest__.py
+++ addons/lsr/__manifest__.py
@@ -39,11 +39,11 @@ Long description of module's purpose
     'demo': [],
     'assets': {
         'web.assets_backend': [          
-            'https://fonts.googleapis.com/css2?family=Prompt:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap'
+            'lsr/static/src/scss/prompt.scss'
         ],
         'web.assets_frontend': [          
             'lsr/static/src/scss/theme.scss',
-            'https://fonts.googleapis.com/css2?family=Prompt:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap'
+            'lsr/static/src/scss/prompt.scss'
         ],
         'web._assets_primary_variables': [
             ('after', 'website/static/src/scss/primary_variables.scss','lsr/static/src/scss/colortemplete.scss'),
